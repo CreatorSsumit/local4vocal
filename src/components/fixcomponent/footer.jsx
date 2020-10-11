004f7f9226201adaae1951196b7949284013ace2
sumit <pksumitkumar98@gmail.com>
2020-10-11 17:59:01 +0530

    sF

--- src/components/fixcomponent/footer.jsx
+++ src/components/fixcomponent/footer.jsx
@@ -47,7 +47,7 @@ const footer = () => {
  
 
   <div class="footer-copyright text-center py-3 text-white">© 2020 Copyright:
-    <a href="https://mdbootstrap.com/"> CreatorSsumit </a>
+    <a style={{color:"orange"}} href="https://mdbootstrap.com/"> CreatorSsumit </a>
   </div>
 
 </footer>
